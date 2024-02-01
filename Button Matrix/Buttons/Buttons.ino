07036fdf85961a80ae9ec1daf0cbf5a097662d38
Zorin Egor <zorin-ep@mail.ru>
2024-02-01 10:11:25 +0800

    test one button

--- Button Matrix/Buttons/Buttons.ino
+++ Button Matrix/Buttons/Buttons.ino
@@ -1,7 +1,30 @@
-void setup() {
+int buttonState = 0;
+int buttonPreviousState = LOW;
+bool buttonPressed = false;
 
+void setup()
+{
+  pinMode(2, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 }
 
-void loop() {
+void loop()
+{
+  buttonState = digitalRead(2);
+  if (buttonState == HIGH) {
+    buttonPressed = true;
+  } else {
+    buttonPressed = false;
+  }
 
+  if (buttonPressed) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("Button pressed!");
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+
+  buttonPreviousState = buttonState;
+  delay(10);
 }
