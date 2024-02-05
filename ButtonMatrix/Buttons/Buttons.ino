aeb4eecb20b670b5cedaeaf9c89285fae96902a1
Zorin Egor <zorin-ep@mail.ru>
2024-02-05 21:33:14 +0800

    refactor

--- ButtonMatrix/Buttons/Buttons.ino
+++ ButtonMatrix/Buttons/Buttons.ino
@@ -1,42 +1,39 @@
-const int outputPins[] = {8, 9};
-const int inputPins[] = {10, 11};
-int outputStates[][2] = {{LOW, HIGH}, {HIGH, LOW}};
-int prevInputStatus[4] = {0};
-int inputStatus[4] = {0};
+const int NUM_OUTPUT_PINS = 2;
+const int NUM_INPUT_PINS = 2;
+const int DELAY_TIME = 300;
 
-void setup() {
-  for (int i = 0; i < 2; i++) {
+const int outputPins[NUM_OUTPUT_PINS] = {8, 9};
+const int inputPins[NUM_INPUT_PINS] = {10, 11};
+const int outputStates[][NUM_OUTPUT_PINS] = {{LOW, HIGH}, {HIGH, LOW}};
+int prevInputStatus[NUM_INPUT_PINS * NUM_OUTPUT_PINS] = {0};
+int inputStatus[NUM_INPUT_PINS * NUM_OUTPUT_PINS] = {0};
+
+void initializePins() {
+  for (int i = 0; i < NUM_OUTPUT_PINS; i++) {
     pinMode(outputPins[i], OUTPUT);
     pinMode(inputPins[i], INPUT_PULLUP);
   }
-  Serial.begin(9600);
 }
 
-void changeInputStatus(int index) {
-  for (int i = 0; i < 2; i++) {
-    inputStatus[index + i] = !digitalRead(inputPins[i]);
+void printInputStatus() {
+  Serial.print("Input status: ");
+  for (int& value : inputStatus) {
+    Serial.print(value);
   }
-
-  boolean hasChanged = false;
-  for (int i = 0; i < 4; i++) {
-    if (memcmp(inputStatus, prevInputStatus, sizeof(inputStatus)) != 0) {
-  hasChanged = true;
+  Serial.println();
 }
 
+void updateInputStatus(int index) {
+  for (int i = 0; i < NUM_INPUT_PINS; i++) {
+    inputStatus[(index * NUM_INPUT_PINS) + i] = !digitalRead(inputPins[i]);
   }
-
-  if (hasChanged && !allZeros(inputStatus, 4)) {
-    memcpy(prevInputStatus, inputStatus, sizeof(prevInputStatus));
-    Serial.print("Input status: ");
-    for (int value : inputStatus) {
-      Serial.print(value);
-    }
-    Serial.println();
-  }
-  delay(300);
 }
 
-boolean allZeros(int arr[], int size) {
+bool hasInputChanged() {
+  return memcmp(inputStatus, prevInputStatus, sizeof(inputStatus)) != 0;
+}
+
+bool allZeros(int arr[], int size) {
   for (int i = 0; i < size; i++) {
     if (arr[i] != 0) {
       return false;
@@ -45,10 +42,27 @@ boolean allZeros(int arr[], int size) {
   return true;
 }
 
+void updateOutputPins(int index) {
+  for (int i = 0; i < NUM_OUTPUT_PINS; i++) {
+    digitalWrite(outputPins[i], outputStates[index][i]);
+  }
+}
+
+void setup() {
+  initializePins();
+  Serial.begin(9600);
+}
+
 void loop() {
-  for (int i = 0; i < 2; i++) {
-    digitalWrite(outputPins[0], outputStates[i][0]);
-    digitalWrite(outputPins[1], outputStates[i][1]);
-    changeInputStatus(i * 2);
+  for (int i = 0; i < NUM_OUTPUT_PINS; i++) {
+    updateOutputPins(i);
+    updateInputStatus(i);
+
+    if (hasInputChanged() && !allZeros(inputStatus, NUM_INPUT_PINS * NUM_OUTPUT_PINS)) {
+      memcpy(prevInputStatus, inputStatus, sizeof(prevInputStatus));
+      printInputStatus();
+    }
+    
+    delay(DELAY_TIME);
   }
 }
