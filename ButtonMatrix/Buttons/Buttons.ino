97034b3625bccb2aace55c39a7950379a694d653
Zorin Egor <zorin-ep@mail.ru>
2024-02-05 21:35:42 +0800

    add comments

--- ButtonMatrix/Buttons/Buttons.ino
+++ ButtonMatrix/Buttons/Buttons.ino
@@ -8,6 +8,7 @@ const int outputStates[][NUM_OUTPUT_PINS] = {{LOW, HIGH}, {HIGH, LOW}};
 int prevInputStatus[NUM_INPUT_PINS * NUM_OUTPUT_PINS] = {0};
 int inputStatus[NUM_INPUT_PINS * NUM_OUTPUT_PINS] = {0};
 
+// Инициализация пинов в режиме ввода/вывода
 void initializePins() {
   for (int i = 0; i < NUM_OUTPUT_PINS; i++) {
     pinMode(outputPins[i], OUTPUT);
@@ -15,6 +16,7 @@ void initializePins() {
   }
 }
 
+// Вывод текущего состояния входов в Serial Monitor
 void printInputStatus() {
   Serial.print("Input status: ");
   for (int& value : inputStatus) {
@@ -23,16 +25,19 @@ void printInputStatus() {
   Serial.println();
 }
 
+// Обновление состояния входов
 void updateInputStatus(int index) {
   for (int i = 0; i < NUM_INPUT_PINS; i++) {
     inputStatus[(index * NUM_INPUT_PINS) + i] = !digitalRead(inputPins[i]);
   }
 }
 
+// Проверка, изменилось ли состояние входов
 bool hasInputChanged() {
   return memcmp(inputStatus, prevInputStatus, sizeof(inputStatus)) != 0;
 }
 
+// Проверка, все ли элементы массива равны нулю
 bool allZeros(int arr[], int size) {
   for (int i = 0; i < size; i++) {
     if (arr[i] != 0) {
@@ -42,6 +47,7 @@ bool allZeros(int arr[], int size) {
   return true;
 }
 
+// Обновление состояния выходов
 void updateOutputPins(int index) {
   for (int i = 0; i < NUM_OUTPUT_PINS; i++) {
     digitalWrite(outputPins[i], outputStates[index][i]);
@@ -49,15 +55,20 @@ void updateOutputPins(int index) {
 }
 
 void setup() {
+  // Инициализация пинов и Serial
   initializePins();
   Serial.begin(9600);
 }
 
 void loop() {
   for (int i = 0; i < NUM_OUTPUT_PINS; i++) {
+    // Установка состояния выходов
     updateOutputPins(i);
+
+    // Обновление состояния входов и проверка изменений
     updateInputStatus(i);
 
+    // Если состояние входов изменилось и не все значения нулевые, вывод в Serial
     if (hasInputChanged() && !allZeros(inputStatus, NUM_INPUT_PINS * NUM_OUTPUT_PINS)) {
       memcpy(prevInputStatus, inputStatus, sizeof(prevInputStatus));
       printInputStatus();
