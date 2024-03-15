af128d7377e41cf95eb9c28333430f36aaba44dc
Зорин Егор Петрович <zorin-ep@mail.ru>
2024-03-16 01:06:03 +0800

    returned the code

--- H-bridge/H-bridge.ino
+++ H-bridge/H-bridge.ino
@@ -1,41 +1,74 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-
-const char* apSSID = "ESP8266_AP"; 
-const char* apPassword = "12345678"; 
+#define mot1 11
+#define mot2 10
 
 void setup() {
-  Serial.begin(115200);
+  pinMode(mot1, OUTPUT);
+  pinMode(mot2, OUTPUT);
+  analogWrite(mot1, 0);
+  analogWrite(mot2, 0);
+  
+  Serial.begin(9600);
+  Serial.println("H-bridge motor control");
+  Serial.println("Commands: ");
+  Serial.println("1. left <speed>, right <speed> - to change direction and set speed (0-255)");
+  Serial.println("2. stop - to stop");
+}
 
-  // Настройка ESP8266 в режиме точки доступа
-  WiFi.softAP(apSSID, apPassword);
-
-  IPAddress apIP = WiFi.softAPIP();
-  Serial.print("Access Point IP address: ");
-  Serial.println(apIP);
+void motorControl(const char* direction, int speed) {
+  if (strcmp(direction, "right") == 0) {
+    analogWrite(mot1, speed);
+    analogWrite(mot2, 0);
+  } else if (strcmp(direction, "left") == 0) {
+    analogWrite(mot1, 0);
+    analogWrite(mot2, speed);
+  } else if (strcmp(direction, "stop") == 0) {
+    analogWrite(mot1, 0);
+    analogWrite(mot2, 0);
+  } else {
+    Serial.println("Error!!! Invalid direction");
+    return;
+  }
+  
+  Serial.print(">>> Direction: ");
+  Serial.print(direction);
+  Serial.print(", speed: ");
+  Serial.println(speed);
 }
 
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-    
-    // Отправляем GET запрос на указанный URL
-    WiFiClient client;
-    http.begin(client, "http://192.168.4.2:5000/temperature/all?location=home");
-    int httpCode = http.GET();
-    
-    // Если ответ получен успешно
-    if (httpCode > 0) {
-      // Читаем ответ и выводим в Serial Monitor
-      String payload = http.getString();
-      Serial.println(payload);
-    } else {
-      Serial.print("Error on HTTP request: ");
-      Serial.println(http.errorToString(httpCode).c_str());
+  if (Serial.available() > 0) {
+    char input[20];
+    int index = 0;
+    while (Serial.available() > 0) {
+      char character = Serial.read();
+      if (character == '\n') {
+        break;
+      }
+      input[index++] = character;
+      delay(2);
     }
+    input[index] = '\0';
     
-    http.end();
+    char* direction = strtok(input, " ");
+    char* speedStr = strtok(NULL, " ");
+    
+    if (strcmp(direction, "stop") == 0) {
+      motorControl(direction, 0); // Call motorControl with speed 0 for "stop"
+      delay(2000);
+    } else {
+      if (speedStr == NULL) {
+        Serial.println("Error!!! Speed not provided");
+        return;
+      }
+      
+      int speed = atoi(speedStr);
+      if (speed < 0 || speed > 255) {
+        Serial.println("Error!!! Speed out of range (0-255)");
+        return;
+      }
+      
+      motorControl(direction, speed);
+      delay(2000);
+    }
   }
-  
-  delay(5000); 
 }
